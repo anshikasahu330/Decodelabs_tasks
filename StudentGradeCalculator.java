2154811f33cbc5ee4722e4ec41d05af06f32a68c
anshikasahu330 <anshikasahu330@gmail.com>
2026-06-29 13:44:03 +0530

    Added Project 2 - Student Grade Calculator

---
+++ StudentGradeCalculator.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int subjects = sc.nextInt();
+
+        int totalMarks = 0;
+
+        for (int i = 1; i <= subjects; i++) {
+
+            int marks;
+
+            while (true) {
+                System.out.print("Enter marks for Subject " + i + " (0-100): ");
+                marks = sc.nextInt();
+
+                if (marks >= 0 && marks <= 100) {
+                    break;
+                } else {
+                    System.out.println("Invalid marks! Please enter marks between 0 and 100.");
+                }
+            }
+
+            totalMarks += marks;
+        }
+
+        double average = (double) totalMarks / subjects;
+
+        char grade;
+
+        if (average >= 90) {
+            grade = 'A';
+        } else if (average >= 80) {
+            grade = 'B';
+        } else if (average >= 70) {
+            grade = 'C';
+        } else if (average >= 60) {
+            grade = 'D';
+        } else if (average >= 50) {
+            grade = 'E';
+        } else {
+            grade = 'F';
+        }
+
+        System.out.println("\n========== RESULT ==========");
+        System.out.println("Total Marks : " + totalMarks);
+        System.out.printf("Average Percentage : %.2f%%\n", average);
+        System.out.println("Grade : " + grade);
+
+        if (grade == 'F') {
+            System.out.println("Result : FAIL");
+        } else {
+            System.out.println("Result : PASS");
+        }
+
+        sc.close();
+    }
+}
